11568bb0b5ce5c14bf6aeea9db38f7fc4db3ee0b
Will Bennett <william.11bennett@gmail.com>
2017-06-04 13:58:19 -0400

    Update factors.py

--- factors.py
+++ factors.py
@@ -13,7 +13,9 @@ def get_factors(n):
 	for i in range(1, n + 1):
 		if n%i == 0:
 			numFactors += 1
-	return((n, numFactors, round(n/numFactors, 1)))
+	value = round(n/numFactors, 2)
+	print('{0} has {1} factors, with a value of {2}.'.format(n, numFactors, value))
+	return((n, numFactors, value))
 
 def get(n):
 	l = []
